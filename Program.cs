4d1060af515fa767293a441bc84973665ec40973
OMFrydenlund <migfrydenlund@gmail.com>
2024-01-05 12:39:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MethodsArgumentsParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Mig";
+            int age = 10;
+
+            Sarcasm(name, age);
+            /*
+            typing the method content here without extracting, then extracting outside the scope auto pass args
+            and auto sets matching parameters for the extracted method. (Parameters recieve generic names)
+            */
+
+            Console.ReadLine();
+
+        }
+
+        static void Sarcasm(string victimOfSarcasm, int notRealAge)
+        {
+            Console.WriteLine("Well aint that something " + victimOfSarcasm);
+            Console.WriteLine("Apparently you're " + notRealAge + " years old. Go figure");
+        }
+    }
+}
